c01cd98e245231a0e974ba35d827f5cf3772d386
Shikhar Shukla <77640481+shikhar0110@users.noreply.github.com>
2025-04-05 01:11:37 +0530

    Update config.ts

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,11 +1,10 @@
 export const API_CONFIG = {
-     BASE_URL: "https://api.openweathermap.org/data/2.5",
-     GEO: "https://api.openweathermap.org/geo/1.0",
-     API_KEY: import.meta.env.VITE_API_KEY,
-     DEFAULT_PARAMS: {
-       units: "metric",
-       appid: import.meta.env.VITE_API_KEY,
-     },
-   };
+  BASE_URL: "https://api.openweathermap.org/data/2.5",
+  GEO: "https://api.openweathermap.org/geo/1.0",
+  API_KEY: import.meta.env.VITE_API_KEY|| "34e4a424013bb98f8cfe435264ec768f",
+  DEFAULT_PARAMS: {
+    units: "metric",
+    appid: import.meta.env.VITE_API_KEY|| "34e4a424013bb98f8cfe435264ec768f",
+  },
+};
 
-   
